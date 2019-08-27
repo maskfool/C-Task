f1e3bf8eb8de6a6f5d2ef6ab39008144aba6c679
Shubham Saini <shubham1996saini@gmail.com>
2019-08-27 18:57:05 +0530

    Add files via upload

---
+++ DAY1/ConsoleApp1/exercise1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            int convert_1 = Convert.ToInt32(number);
+            int x1 = convert_1 + 5;
+            int convert_2 = Int32.Parse(number);
+            int x2 = convert_2 + 5;
+   
+            Int32.TryParse(number, out convert_3);
+            int x3 = convert_3 + 5;
+
+            Console.Write("\t" + x1);
+            //Console.WriteLine("\t" + x2);
+            //Console.WriteLine("\t" + x3);
+            Console.ReadKey();
+
+
+        }
+    }
+}
